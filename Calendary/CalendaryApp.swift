cd34c3aca6679d4080e35a94e7d41cd374cbe4f3
AryveN <krystofhugo@gmail.com>
2024-04-17 19:25:46 +0200

    Initial Commit

---
+++ Calendary/CalendaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendaryApp.swift
+//  Calendary
+//
+//  Created by Krystof Hugo Maly on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
